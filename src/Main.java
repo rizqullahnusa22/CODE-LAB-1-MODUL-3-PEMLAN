ffd8ad21607842f786c0384948b61f9a6ca97f61
rizqullahnusa22 <rizqullah.nusa22@gmail.com>
2024-10-23 17:13:22 +0700

    daff

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("HALLO DUNIA TIPU-TIPU");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
